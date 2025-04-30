5e94d9e020ffce3b6e338eb259f5a5e24755e89c
Rudransh Shukla <rudranshshukla2211@gmail.com>
2025-04-30 05:55:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > An error occurred in `next/font`.
    >
    > Error: Cannot find module 'autoprefixer'
    > Require stack:
    > - /home/user/studio/node_modules/next/dist/build/webpack/config/blocks/css/plugins.js
    > - /home/user/studio/node_modules/next/dist/build/webpack/config/blocks/css/index.js
    > - /home/user/studio/node_modules/next/dist/build/webpack/config/index.js
    > - /home/user/studio/node_modules/next/dist/build/webpack-config.js
    > - /home/user/studio/node_modules/next/dist/server/dev/hot-reloader-webpack.js
    > - /home/user/studio/node_modules/next/dist/server/lib/router-utils/setup-dev-bundler.js
    > - /home/user/studio/node_modules/next/dist/server/lib/router-server.js
    > - /home/user/studio/node_modules/next/dist/server/lib/start-server.js
    >     at Module._resolveFilename (node:internal/modules/cjs/loader:1212:15)
    >     at /home/user/studio/node_modules/next/dist/server/require-hook.js:55:36
    >     at Function.resolve (node:internal/modules/helpers:193:19)
    >     at loadPlugin (/home/user/studio/node_modules/next/dist/build/webpack/config/blocks/css/plugins.js:49:32)
    >     at /home/user/studio/node_modules/next/dist/build/webpack/config/blocks/css/plugins.js:157:56
    >     at Array.map (<anonymous>)
    >     at getPostCssPlugins (/home/user/studio/node_modules/next/dist/build/webpack/config/blocks/css/plugins.js:157:47)
    >     at async /home/user/studio/node_modules/next/dist/build/webpack/config/blocks/css/index.js:124:36
    >     at async /home/user/studio/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js:86:33
    >     at async Span.traceAsyncFn (/home/user/studio/node_modules/next/dist/trace/trace.js:154:20)

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"; // Assuming Button is created later
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-8">Welcome to Next.js</h1>
+      {/* <Button>Get Started</Button>  Placeholder until Button exists */}
+    </main>
+  );
+}
