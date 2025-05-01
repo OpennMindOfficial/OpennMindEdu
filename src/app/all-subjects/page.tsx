ed3a0de4fdec0a16657ea2ff7a7c423da393100c
Rudransh Shukla <rudranshshukla2211@gmail.com>
2025-05-01 09:25:37 +0000

    i said add. MORE

--- src/app/all-subjects/page.tsx
+++ src/app/all-subjects/page.tsx
@@ -71,7 +71,7 @@ export default function AllSubjectsPage() {
           </div>
 
           {/* Increased grid gap for more uniform spacing */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[20px]">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[30px]">
             {allSubjects.map((subject, index) => (
               <SubjectCard
                 key={index}
